c0d690b09405854ac2089537e64ba7a204519dc8
oneofthedevs <iamdevchhaniyara@gmail.com>
2020-09-28 14:14:53 +0530

    :heavy_plus_sign: :heavy_plus_sign: expressn and nodemon

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 // Add type: "module" in package.json to use ES6 syntex
 //import { createServer } from "http";
 
-const http = require("http");
 const express = require("express");
+const app = express();
 const host = "localhost";
 const port = "2024";
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello from dark side");
-});
+app.get("/", (res, req) => {});
 
-server.listen(port, host, () => {
-  console.log(`Server is running at ${host}:${port}`);
+app.listen(port, host, () => {
+  console.log(`Server running on http://${host}:${port}`);
 });
---
+++ http.js
@@ -0,0 +1,16 @@
+// Add type: "module" in package.json to use ES6 syntex
+//import { createServer } from "http";
+
+const http = require("http");
+const host = "localhost";
+const port = "2024";
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello from dark side");
+});
+
+server.listen(port, host, () => {
+  console.log(`Server is running at ${host}:${port}`);
+});
