68a31501e1fa747fad0da91ce6a15905c357e493
oneofthedevs <iamdevchhaniyara@gmail.com>
2020-09-28 11:57:37 +0530

    :tada: Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const host = "localhost";
+const port = "2024";
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello from dark side");
+});
+
+server.listen(port, host, () => {
+  console.log(`Server is running at ${host}:${port}`);
+});
