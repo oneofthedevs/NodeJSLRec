ad697e5f16dab7f67d69d2758333dc7684e22240
oneofthedevs <iamdevchhaniyara@gmail.com>
2020-09-30 15:32:46 +0530

    :heavy_plus_sign: redis and :sparkles: :construction: Implementing redis publish and subscribe

---
+++ Subscriber/server.js
@@ -0,0 +1,19 @@
+// const express = require("express");
+const redis = require("redis");
+
+// const app = express();
+const subscriber = redis.createClient();
+
+subscriber.on("message", (channel, message) => {
+  console.log(`Message: ${message}, Channel: ${channel}`);
+});
+
+subscriber.subscribe("user-notify");
+
+// app.get("/", (req, res) => {
+//   res.send("Subs one");
+// });
+
+// app.listen(3000, () => {
+//   console.log("Port 3000");
+// });
---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const redis = require("redis");
+
+const app = express();
+const publisher = redis.createClient();
+
+app.get("/", (req, res) => {
+  return res.send("Hello from Dark side");
+});
+
+app.get("/publish", (req, res) => {
+  const user = {
+    id: Math.floor(Math.random() * 1000),
+    name: "Obi-Wan",
+  };
+  let publisher = redis.createClient();
+  publisher.publish("user-notify", JSON.stringify(user), () => {
+    process.exit(0);
+  });
+  return res.send("Message Published");
+});
+
+// app.get("/", (req, res) => {
+//   const user = {
+//     id: Math.floor(Math.random() * 1000),
+//     name: "Obi-Wan",
+//   };
+
+//   publisher.publish("user-notify", JSON.stringify(user));
+//   res.send("Publishing event using redis");
+// });
+
+// app.listen(4000, () => {
+//   console.log("Port: 4000");
+// });
